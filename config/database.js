0fe16c95c5c3ec00a5857e014dd579811757cda0
kyoungd <kyoungd@hotmail.com>
2022-01-15 23:43:07 -0800

    heroku changes

--- config/database.js
+++ config/database.js
@@ -1,11 +1,34 @@
-const path = require('path');
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'simp-admin'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'Service$11'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
 
+// postgres://{user}:{password}@{hostname}:{port}/{database-name}
+// DATABASE_URL=postgres://postgres:Service$11@127.0.0.1:5432/simp-admin
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
