aaceed766bc751c9d533a305ba511c1ba6d2ac53
kyoungd <kyoungd@hotmail.com>
2022-02-20 00:51:05 -0800

    updated favorites create API

--- src/api/favorite/controllers/favorite.js
+++ src/api/favorite/controllers/favorite.js
@@ -25,12 +25,39 @@ module.exports = createCoreController('api::favorite.favorite', ({ strapi }) => 
         return { data, meta };
     },
     
+    async create(ctx) {
+        const { id } = ctx.state.user;
+        const { body } = ctx.request;
+
+        const entry = await strapi.db.query('api::favorite.favorite').findOne({
+            where: { user: id },
+        });
+        if (entry) {
+            const entity = await strapi.entityService.update('api::favorite.favorite', id, {
+                data: {
+                    data: body,
+                },
+            });
+            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+            return this.transformResponse(sanitizedEntity);
+        } else {
+            const entity2 = await strapi.db.query('api::favorite.favorite').create({
+                data: {
+                    data: body,
+                    user: id
+                },
+            });
+            const sanitizedEntity = await this.sanitizeOutput(entity2, ctx);
+            return this.transformResponse(sanitizedEntity);
+        }
+    },
+
     async update(ctx) {
         const { id } = ctx.state.user;
         const { body } = ctx.request;
 
         const entry = await strapi.db.query('api::favorite.favorite').findOne({
-            where: { user: id + 5 },
+            where: { user: id },
         });
         if (entry) {
             const entity = await strapi.entityService.update('api::favorite.favorite', id, {
