152c9c824394200549307813871f251c40f1e319
kyoungd <kyoungd@hotmail.com>
2022-02-26 21:56:25 -0800

    v4 plugin.js file

--- config/plugin.js
+++ config/plugin.js
@@ -1,13 +1,16 @@
 module.exports = ({ env }) => ({
     // ...
     email: {
-        provider: 'sendgrid',
-        providerOptions: {
-            apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-            defaultFrom: env('SENDGRID_EMAIL_FROM', 'young@ecom-live.com'),
-            defaultReplyTo: env('SENDGRID_REPLY_TO', 'young@ecom-live.com'),
+        config: {
+            provider: 'sendgrid',
+            providerOptions: {
+                apiKey: env('SENDGRID_API_KEY'),
+            },
+            settings: {
+                defaultFrom: env('SENDGRID_EMAIL_FROM', 'young@ecom-live.com'),
+                defaultReplyTo: env('SENDGRID_REPLY_TO', 'young@ecom-live.com'),
+                testAddress: 'kyoungd@hotmail.com',
+            },
         },
     },
     // ...
