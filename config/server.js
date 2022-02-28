0afa897a7f7010bafb1e13fe8f385d0a3f95e74c
kyoungd <kyoungd@hotmail.com>
2022-02-28 11:11:45 -0800

    server.js update 2

--- config/server.js
+++ config/server.js
@@ -2,7 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   proxy: true,
-  url: "https://simp-admin.herokuapp.com",
   app: {
     keys: env.array("APP_KEYS"),
   },
