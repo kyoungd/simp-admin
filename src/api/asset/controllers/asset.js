59debc4179dc8569e381bc9fa63dddd6d65ef9ab
kyoungd <kyoungd@hotmail.com>
2022-03-08 16:35:51 -0800

    adding stock chart data update

--- src/api/asset/controllers/asset.js
+++ src/api/asset/controllers/asset.js
@@ -20,11 +20,7 @@ module.exports = createCoreController('api::asset.asset', ({ strapi }) =>({
         return this.transformResponse(sanitizedEntity);
     },
 
-    async create(ctx) {
-        const { query } = ctx;
-        const { body } = ctx.request;
-
-        const { symbol, timeframe} = query;
+    async updateDb(symbol, timeframe, body) {
         const entry = await strapi.db.query('api::asset.asset').findOne({
             where: {
                 symbol: symbol,
@@ -37,8 +33,7 @@ module.exports = createCoreController('api::asset.asset', ({ strapi }) =>({
                     data: body,
                 },
             });
-            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
-            return this.transformResponse(sanitizedEntity);
+            return true;
         } else {
             const entity2 = await strapi.db.query('api::asset.asset').create({
                 data: {
@@ -47,9 +42,49 @@ module.exports = createCoreController('api::asset.asset', ({ strapi }) =>({
                     timeframe: timeframe
                 },
             });
-            const sanitizedEntity = await this.sanitizeOutput(entity2, ctx);
-            return this.transformResponse(sanitizedEntity);
+            return true;
         }
+        return false;
+    },
+
+    async create(ctx) {
+        const { body } = ctx.request;
+
+        let failCount = 0;
+        for (const entry of body) {
+            const { symbol, timeframe, data } = entry;
+            const isUpdateOk = await this.updateDb(symbol, timeframe, data);
+            if (!isUpdateOk)
+                ++failCount;
+        }
+        const result = { failure: failCount, success: body.length - failCount };
+        const sanitizedEntity = await this.sanitizeOutput(result, ctx);
+        return this.transformResponse(sanitizedEntity);
+        // const entry = await strapi.db.query('api::asset.asset').findOne({
+        //     where: {
+        //         symbol: symbol,
+        //         timeframe: timeframe
+        //     },
+        // });
+        // if (entry) {
+        //     const entity = await strapi.entityService.update('api::asset.asset', entry.id, {
+        //         data: {
+        //             data: body,
+        //         },
+        //     });
+        //     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+        //     return this.transformResponse(sanitizedEntity);
+        // } else {
+        //     const entity2 = await strapi.db.query('api::asset.asset').create({
+        //         data: {
+        //             data: body,
+        //             symbol: symbol,
+        //             timeframe: timeframe
+        //         },
+        //     });
+        //     const sanitizedEntity = await this.sanitizeOutput(entity2, ctx);
+        //     return this.transformResponse(sanitizedEntity);
+        // }
     },
 
 }));
