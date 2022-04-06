44df1639be24aac3f358acf336496dd6fa7258ff
kyoungd <kyoungd@hotmail.com>
2022-04-05 17:19:08 -0700

    added all favorite return for sysops

--- src/api/favorite/controllers/favorite.js
+++ src/api/favorite/controllers/favorite.js
@@ -11,13 +11,27 @@ module.exports = createCoreController('api::favorite.favorite', ({ strapi }) => 
     async find(ctx) {
         const { id } = ctx.state.user;
         // some logic here
+        if (ctx.state.user.role.type === 'sysops') {
+            const { data, meta } = await super.find(ctx);
+            const symbols = {};
+            data.forEach(item => {
+                const row = item.attributes.data;
+                for (let key in row) {
+                    if (!symbols.hasOwnProperty(key)) {
+                        symbols[key] = 0;
+                    }
+                }
+            });
+            const sanitizedEntity = await this.sanitizeOutput(symbols, ctx);
+            return this.transformResponse(sanitizedEntity);
+        }
         const entity = await strapi.db.query('api::favorite.favorite').findOne({
             where: { user: id },
         });
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
         return this.transformResponse(sanitizedEntity);
     },
-    
+
     async create(ctx) {
         const { id } = ctx.state.user;
         const { body } = ctx.request;
